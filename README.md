ce74ee4e33a0ab0b7549e2389419f1979c9ffad5
Jay Crossler <jay.crossler@gmail.com>
2014-01-01 16:52:50 -0500

    First build

---
+++ README.md
@@ -0,0 +1,26 @@
+Overview
+========
+
+This is a set of Chef recipes (think of them as macros to automatically build a running Virtual Machine) that will work to set the
+geoevents app up on either a local Virtualbox VM or onto an Amazon Web Service VM.
+
+
+Configuration
+=============
+
+1. Download VirtualBox and install it (free at https://www.virtualbox.org/)
+2. Download Vagrant and install it (free at http://www.vagrantup.com/downloads)
+3. Download Git and install it (free at )
+
+4. Open a command line, and 'cd' to or create a directory to keep your source code. I use a directory named Sites for example that's in my home directory (~/Sites)
+5. Checkout this chef repo to your local machine:
+# git clone https://github.com/jaycrossler/procyon-chef-repo.git
+
+6. cd procyon-chef-repo
+7. (If you want to deploy to Amazon or some other provider, create a file named 'vagrant_dev_settings.yml' in the code directory (i.e. ~/Sites) and add in provider deployment details.
+8. Have Vagrant create the VM:
+# vagrant up --provider=aws (if using AWS, and the vagrant_dev_settings.yml were created), or:
+# vagrant up (to deploy to a local virtualbox vm)
+
+9. After it's up and provisioned you should be able to either 'vagrant ssh' to it, or to go to the url in a web browser.
+You can type 'vagrant provision' to have it update code from github and install new libraries, or 'vagrant halt' to stop the VM.
---
+++ cookbooks/geoevents/recipes/default.rb
@@ -0,0 +1,105 @@
+geoevents_pkgs = "build-essential python-dev libpq-dev libpng-dev libfreetype6 libfreetype6-dev".split
+
+geoevents_pkgs.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+python_virtualenv node['geoevents']['virtualenv']['location'] do
+  interpreter "python2.7"
+  action :create
+end
+
+python_pip "uwsgi" do
+  virtualenv node['geoevents']['virtualenv']['location']
+end
+
+git node['geoevents']['location'] do
+  repository node['geoevents']['git_repo']['location']
+  revision node['geoevents']['git_repo']['branch']
+  action :sync
+  notifies :run, "execute[install_geoevents_dependencies]", :immediately
+  notifies :run, "bash[sync_db]"
+  notifies :run, "execute[collect_static]"
+end
+
+execute "install_geoevents_dependencies" do
+  command "#{node['geoevents']['virtualenv']['location']}/bin/pip install -r geoevents/requirements.txt"
+  cwd node['geoevents']['location']
+  action :nothing
+  user 'root'
+end
+
+template "geoevents_local_settings" do
+  source "local_settings.py.erb"
+  path "#{node['geoevents']['virtualenv']['location']}/local_settings.py"
+  variables ({:database => node['geoevents']['settings']['DATABASES']['default']})
+end
+
+link "local_settings_symlink" do
+  link_type :symbolic
+  to "#{node['geoevents']['virtualenv']['location']}/local_settings.py"
+  target_file "#{node['geoevents']['location']}/geoevents/local_settings.py"
+  not_if do File.exists?("#{node['geoevents']['location']}/geoevents/local_settings.py") end
+end
+
+hostsfile_entry node['geoevents']['database']['address'] do
+  hostname node['geoevents']['database']['hostname']
+  only_if do node['geoevents']['database']['hostname'] && node['geoevents']['database']['address'] end
+  action :append
+end
+
+include_recipe 'geoevents::postgis'
+include_recipe 'geoevents::database'
+
+directory node['geoevents']['logging']['location'] do
+  action :create
+end
+
+directory node['geoevents']['settings']['static_root'] do
+  owner "root"
+  mode 00755
+  action :create
+  recursive true
+end
+
+bash "sync_db" do
+  code "source #{node['geoevents']['virtualenv']['location']}/bin/activate && paver sync"
+  cwd "#{node['geoevents']['location']}"
+  action :nothing
+end
+
+execute "collect_static" do
+  command "#{node['geoevents']['virtualenv']['location']}/bin/python manage.py collectstatic --noinput"
+  cwd "#{node['geoevents']['location']}"
+  action :nothing
+end
+
+bash "install_fixtures" do
+  code "source #{node['geoevents']['virtualenv']['location']}/bin/activate && paver install_dev_fixtures"
+  cwd "#{node['geoevents']['location']}"
+  user 'postgres'
+  action :nothing
+end
+
+template "geoevents_uwsgi_ini" do
+  path "#{node['geoevents']['virtualenv']['location']}/geoevents.ini"
+  source "geoevents.ini.erb"
+  action :create_if_missing
+  notifies :run, "execute[start_django_server]"
+end
+
+include_recipe 'geoevents::nginx'
+
+start_geoevents = "#{node['geoevents']['virtualenv']['location']}/bin/uwsgi --ini #{node['geoevents']['virtualenv']['location']}/geoevents.ini &"
+
+execute "start_django_server" do
+  command start_geoevents
+end
+
+file "/etc/cron.d/geoevents_restart" do
+  content "@reboot root #{node['geoevents']['virtualenv']['location']}/bin/uwsgi --ini #{node['geoevents']['virtualenv']['location']}/geoevents.ini &"
+  mode 00755
+  action :create_if_missing
+end
